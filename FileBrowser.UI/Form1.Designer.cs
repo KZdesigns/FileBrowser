277f9cf0228141f67f6f3bd2f134fbbf96798b59
Kyle Zimmerman <kaz0010@auburn.com>
2022-04-09 17:42:59 -0700

    UI complete

--- FileBrowser.UI/Form1.Designer.cs
+++ FileBrowser.UI/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblSelectAFile = new System.Windows.Forms.Label();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxSearchPhrase = new System.Windows.Forms.TextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblSelectAFile
+            // 
+            this.lblSelectAFile.AutoSize = true;
+            this.lblSelectAFile.Location = new System.Drawing.Point(78, 61);
+            this.lblSelectAFile.Name = "lblSelectAFile";
+            this.lblSelectAFile.Size = new System.Drawing.Size(68, 13);
+            this.lblSelectAFile.TabIndex = 0;
+            this.lblSelectAFile.Text = "Select a File:";
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(81, 89);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(78, 23);
+            this.buttonOpenFile.TabIndex = 1;
+            this.buttonOpenFile.Text = "Open";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(153, 61);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(119, 13);
+            this.lblFileName.TabIndex = 2;
+            this.lblFileName.Text = "Replace with File Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Search For Keyword or Phrase:";
+            // 
+            // textBoxSearchPhrase
+            // 
+            this.textBoxSearchPhrase.Location = new System.Drawing.Point(84, 179);
+            this.textBoxSearchPhrase.Name = "textBoxSearchPhrase";
+            this.textBoxSearchPhrase.Size = new System.Drawing.Size(413, 20);
+            this.textBoxSearchPhrase.TabIndex = 4;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(84, 206);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 5;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.Location = new System.Drawing.Point(84, 269);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(635, 160);
+            this.listBoxResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.textBoxSearchPhrase);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.buttonOpenFile);
+            this.Controls.Add(this.lblSelectAFile);
+            this.Name = "Form1";
+            this.Text = "File Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblSelectAFile;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxSearchPhrase;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listBoxResult;
     }
 }
 
